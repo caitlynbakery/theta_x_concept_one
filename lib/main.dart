fe2c90c975d0af0642bbdacdc41b3496e86fc00e
Caitlyn <caitlyn@oppkey.com>
2022-06-07 10:09:53 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'THETA X',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40,
+                  fontFamily: 'Questrial',
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            const Text(
+              'BUTTON CONTROLS',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 27, fontFamily: 'Questrial'),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      print('press info!');
+                      var url = Uri.parse('http://192.168.1.1/osc/info');
+                      var header = {
+                        'Content-Type': 'application/json;charset=utf-8'
+                      };
+                      var response = await http.get(url, headers: header);
+                      print(response.body);
+                    },
+                    child: const Text('Info'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shadowColor: Colors.grey,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      print("press state!");
+                      var url = Uri.parse('http://192.168.1.1/osc/state');
+                      var header = {
+                        'Content-Type': 'application/json;charset=utf-8'
+                      };
+                      var response = await http.post(url, headers: header);
+                      print(response.body);
+                      //   print(response.body[0]);
+                    },
+                    child: const Text('State'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shadowColor: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    print("press state!");
+                    var url =
+                        Uri.parse('http://192.168.1.1/osc/commands/execute');
+                    var header = {
+                      'Content-Type': 'application/json;charset=utf-8'
+                    };
+                    var response = await http.post(url, headers: header);
+                    print(response.body);
+                    //  print(response.body[0]);
+                  },
+                  child: const Text('Take Pic'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.black38,
+                      shadowColor: Colors.grey,
+                      fixedSize: Size(60, 60),
+                      shape: CircleBorder()),
+                ),
+              ],
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
